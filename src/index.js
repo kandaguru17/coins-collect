31e6ebb95ac88d2c4804ade19a210170c3798cf6
kandaguru <kandaguru@tradewindow.io>
2020-10-06 11:03:35 +1300

    Made changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='dapp'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
